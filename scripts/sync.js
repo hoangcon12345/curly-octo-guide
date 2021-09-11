00eca3fac3ee7eb94666e644472148244786e8c5
hoangcon12345 <44178312+hoangcon12345@users.noreply.github.com>
2021-09-12 01:56:50 +0700

    Initialize build-fail-versions-ci

---
+++ scripts/sync.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const program = require('commander');
+const fs = require('fs-extra')
+const config = require('../lib/config')
+const util = require('../lib/util')
+
+// TEMP
+console.log('IS CI Envorinment?', isCIEnvironment)
+process.exit(1)
+// TEMP
+
+const projectNames = config.projectNames.filter((project) => config.projects[project].ref)
+
+program
+  .version(process.env.npm_package_version)
+  .option('--gclient_file <file>', 'gclient config file location')
+  .option('--gclient_verbose', 'verbose output for gclient')
+  .option('--run_hooks', 'run gclient hooks')
+  .option('--run_sync', 'run gclient sync')
+  .option('--target_os <target_os>', 'target OS')
+  .option('--target_arch <target_arch>', 'target architecture')
+  .option('--target_apk_base <target_apk_base>', 'target Android OS apk (classic, modern, mono)')
+  .option('--submodule_sync', 'run submodule sync')
+  .option('--init', 'initialize all dependencies')
+  .option('--all', 'update all projects')
+projectNames.forEach((name) => {
+  let project = config.projects[name]
+  program.option('--' + project.arg_name + '_ref <ref>', name + ' ref to checkout')
+})
+
+program.parse(process.argv)
+config.update(program)
+
+if (program.init || program.submodule_sync) {
+  util.submoduleSync()
+}
+
+if (program.init) {
+  util.buildGClientConfig()
+}
+
+if (program.init) {
+  util.gclientSync(true)
+}
+
+let updatedVersion = false
+
+projectNames.forEach((name) => {
+  let project = config.projects[name]
+  if (program.init || program.all || program[project.arg_name + '_ref']) {
+    updatedVersion = true
+    util.setDepVersion(project.dir, project.ref)
+  }
+})
+
+if (updatedVersion || program.init || program.run_sync) {
+  util.gclientSync(program.init)
+}
+
+if (updatedVersion || program.init || program.run_hooks) {
+  const core_dir = config.projects['brave-core'].dir
+  util.run('python', [path.join(core_dir, 'script', 'apply-patches.py')])
+  util.gclientRunhooks()
+}
